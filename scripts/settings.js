3810b666878b43e2ab9d73160779026c84299c6f
Justin Ross <justin@thsjst.in>
2023-02-01 11:06:10 -0800

    Updated settings labels

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,7 +1,7 @@
 export const registerSystemSettings = async function() {
 
   await game.settings.register('bitd-alternate-sheets', 'populateFromCompendia', {
-    name: 'Load Compendium Playbooks',
+    name: 'Load Compendium Objects',
     hint: 'Include Compendium Playbooks, NPCs, Items, and Abilities When Auto-Populating Playbooks',
     scope: 'world',     // "world" = sync to db, "client" = local storage
     config: true,       // false if you dont want it to show in module config
@@ -12,7 +12,7 @@ export const registerSystemSettings = async function() {
   });
 
   await game.settings.register('bitd-alternate-sheets', 'populateFromWorld', {
-    name: 'Load World Playbooks',
+    name: 'Load World Objects',
     hint: 'Include World (Custom) Playbooks, NPCs, Items, and Abilities When Auto-Populating Playbooks',
     scope: 'world',     // "world" = sync to db, "client" = local storage
     config: true,       // false if you dont want it to show in module config
